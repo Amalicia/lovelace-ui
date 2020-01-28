89e9bd4ea1a8e9aaf1b0276c1f838d088c5c332b
Lottie Bryant <amaliciabryant@gmail.com>
2020-01-28 16:46:16 +0000

    Initial stages of figuring out how to do front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import {Spinner} from 'react-bootstrap'
+import cow from './images/cow_logo.png';
+import fish from './images/fish.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="Nav">
+          <Navbar bg="dark" variant="dark" fixed="top">
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src={cow}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{' '}
+              Lovelace
+              </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="about">About</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={fish} className="App-logo" alt="logo" />
+          <p>
+            This is a work in progress...
+          </p>
+          <Spinner animation="border"></Spinner>
+        </header>
+      </div>
     </div>
   );
 }
