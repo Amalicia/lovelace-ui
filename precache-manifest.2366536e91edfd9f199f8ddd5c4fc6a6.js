491273cc470050e1b538a882dd07a0cb78f0a66a
Lottie Bryant <amaliciabryant@gmail.com>
2020-01-28 15:55:05 +0000

    Updates

---
+++ precache-manifest.2366536e91edfd9f199f8ddd5c4fc6a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1745f5ed5e43eab19c75109252bc2d",
+    "url": "/lovelace-ui/index.html"
+  },
+  {
+    "revision": "ccbbdb62d286f264fdfa",
+    "url": "/lovelace-ui/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ab84d672141b1debf67d",
+    "url": "/lovelace-ui/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ccbbdb62d286f264fdfa",
+    "url": "/lovelace-ui/static/js/2.4c85dfae.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/lovelace-ui/static/js/2.4c85dfae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab84d672141b1debf67d",
+    "url": "/lovelace-ui/static/js/main.16728d8d.chunk.js"
+  },
+  {
+    "revision": "384666287435ce3e8c65",
+    "url": "/lovelace-ui/static/js/runtime-main.84ca4a25.js"
+  },
+  {
+    "revision": "d62437d41f27e396bfc1f10ec39ee99a",
+    "url": "/lovelace-ui/static/media/fish.d62437d4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lovelace-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lovelace-ui/precache-manifest.2366536e91edfd9f199f8ddd5c4fc6a6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lovelace-ui/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
