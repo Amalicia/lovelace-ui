20a9cee47cd4b9bce589181e9ff62edd64c8849c
Lottie Bryant <amaliciabryant@gmail.com>
2020-01-31 10:02:39 +0000

    further refactoring and understanding

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import cow from './cow_logo.png';
+
+const NavBar = () => (
+    <div className="Nav">
+      <Navbar bg="dark" variant="dark" fixed="top">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={cow}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Lovelace
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="about">About</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+);
+
+export default NavBar;
